71d35683a8d21341d3f8485d7caa744a3b463583
Skyfe24 <fabio.erase@gmail.com>
2023-07-21 18:22:48 +0200

    added constructor

--- index.php
+++ index.php
@@ -10,22 +10,42 @@ i valori delle relative proprietà                                -->
 <?php
 class Movie
 {
- // Variabili d'istanza
+ // Variabili d'istanza o prorpietà
  public $genre;
  public $lenght;
  public $title;
 
+ // costruttore
+ public function __construct($genre, $lenght, $title)
+ {       }
+
+ 
 
 
+ // Metodi (funzioni dentro gli oggetti)
+public function presentazioneOggi(){
+    echo 'Buonasera telespettatori, vi annunciamo che andrà ora in onda' . $this->title;
+    }
+public function presentazioneDomani(){
+    echo 'Buonasera telespettatori, abbiamo il piacere di anticiparvi 
+        che domani sera trasmetteremo il film' . $this->title;
+    }
 
 
 }
 
-$orainonda = new Movie();
-$domaniserainTv = new Movie();
+$orainonda = new Movie("Storico", "2 ore e 5 minuti", "Caccia all'Ottobre Rosso");
+$domaniserainTv = new Movie("Commedia", "1 ora e 48 minuti", "Mamma ho Perso l'Aereo" );
 
 $orainonda-> title = "Caccia all'Ottobre Rosso";
 $domaniserainTv-> title = "Mamma ho Perso l'Aereo";
+$orainonda-> genre = "Storico";
+$domaniserainTv-> genre = "Commedia";
+$orainonda-> lenght = "2 ore e 5 minuti";
+$domaniserainTv-> lenght = "1 ora e 48 minuti";
+
+$orainonda -> presentazioneOggi();
+$domaniserainTv -> presentazioneDomani();
 
 var_dump($orainonda);
 var_dump($domaniserainTv);
